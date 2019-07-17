cf939c22149b319abc474e72ccd1c8c96d1caad2
SukJinKim <rkathrwks@gmail.com>
2019-07-17 15:04:30 +0900

    Add URLManager.java

---
+++ src/main/java/isel/jira/URLManager.java
@@ -0,0 +1,18 @@
+package isel.jira;
+
+public class URLManager {
+	private String domain;
+	
+	private static final String URL_FRAGMENT1 = "https://";
+	private static final String URL_FRAGMENT2 = "/sr/jira.issueviews:searchrequest-csv-all-fields/temp/SearchRequest.csv?jqlQuery=";
+	
+	public URLManager(String domain) {
+		super();
+		this.domain = domain;
+	}
+	
+	public String getURL(String encodedJql) {
+		return URL_FRAGMENT1 + this.domain + URL_FRAGMENT2 + encodedJql;
+	}
+
+}
